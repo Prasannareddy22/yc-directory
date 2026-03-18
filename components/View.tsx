7b49617ca853cf2bf287612634999bac52b8e203
Prasannareddy22 <itsmesammi22@gmail.com>
2026-03-18 10:18:20 +0530

    fix view counter and live fetch

--- components/View.tsx
+++ components/View.tsx
@@ -4,26 +4,33 @@ import { STARTUP_VIEWS_QUERY } from "@/sanity/lib/queries";
 import { writeClient } from "@/sanity/lib/write-client";
 
 const View = async ({ id }: { id: string }) => {
-    const { views: totalViews } = await client
-        .withConfig({ useCdn: false })
-        .fetch(STARTUP_VIEWS_QUERY, { id });
+    try {
+        const result = await client
+            .withConfig({ useCdn: false })
+            .fetch(STARTUP_VIEWS_QUERY, { id });
 
-    // Update views
-    await writeClient
-        .patch(id)
-        .set({ views: totalViews + 1 })
-        .commit();
+        const totalViews = result?.views || 0;
 
-    return (
-        <div className="view-container">
-            <div className="absolute -top-2 -right-2">
-                <Ping />
+        // Update views in background
+        writeClient
+            .patch(id)
+            .set({ views: totalViews + 1 })
+            .commit()
+            .catch(console.error);
+
+        return (
+            <div className="view-container">
+                <div className="absolute -top-2 -right-2">
+                    <Ping />
+                </div>
+                <p className="view-text">
+                    <span className="font-black">Views: {totalViews}</span>
+                </p>
             </div>
-
-            <p className="view-text">
-                <span className="font-black">Views: {totalViews}</span>
-            </p>
-        </div>
-    );
+        );
+    } catch (error) {
+        console.error(error);
+        return null;
+    }
 };
 export default View;
